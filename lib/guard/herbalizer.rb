1b7e2a88b6f73e37a50b35c75d76a0787b96e13e
Michael Witrant <michael@ouvrages-web.fr>
2013-10-09 10:56:20 +0200

    generated gem with bundler

---
+++ lib/guard/herbalizer.rb
@@ -0,0 +1,7 @@
+require "guard/herbalizer/version"
+
+module Guard
+  module Herbalizer
+    # Your code goes here...
+  end
+end
---
+++ lib/guard/herbalizer/version.rb
@@ -0,0 +1,5 @@
+module Guard
+  module Herbalizer
+    VERSION = "0.0.1"
+  end
+end
